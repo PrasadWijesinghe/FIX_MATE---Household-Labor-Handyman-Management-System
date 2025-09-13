5927ac89f3af4336a5e14c8f7e1c0d68e1b64466
kavinda dissanayake <121952332+kavinda44@users.noreply.github.com>
2025-09-13 20:47:35 +0530

    pull Error fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,19 +2,15 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
+import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './Context/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <App />
-=======
-import {BrowserRouter} from 'react-router-dom'
-import { AppContextProvider } from './Context/AppContext';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <AppContextProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppContextProvider>
->>>>>>> 1d0a79f8a7f83742971e5aad1a8e6bda3e9570cf
+  </StrictMode>
 )
