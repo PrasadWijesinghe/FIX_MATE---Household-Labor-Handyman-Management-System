5b945959d2e712b97bd82611d36f76072151c1d6
kavinda dissanayake <121952332+kavinda44@users.noreply.github.com>
2025-09-13 20:31:47 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
+import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import AdminDashboard from './admin/AdminDashboard';
 import DashboardMain from './admin/DashboardMain';
 import Users from './admin/Users';
@@ -11,7 +13,8 @@ import Services from './admin/Services';
 
 const App = () => {
   return (
-    <Router>
+    <Router className='app'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/admin" element={<AdminDashboard />}>
