3aa6055eefd847126471ea86656532c436c9b46d
kavinda dissanayake <121952332+kavinda44@users.noreply.github.com>
2025-09-14 23:05:50 +0530

    product section complete

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,17 @@
 
 import logo from './web_logo.png'
 import hero_img from './hero_img.png'
+import service1 from './service1.png'
+import service2 from './service2.png'
+import service3 from './service3.png'
 
 
 export const assets = {
     logo,
-    hero_img
+    hero_img,
+    service1,
+    service2,
+    service3
    
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import './Home.css'
 import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
+import Products from '../../components/Products/Products'
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <Products/>
     </div>
   )
 }
