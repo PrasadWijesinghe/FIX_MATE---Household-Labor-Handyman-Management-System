d8147f8210a89033cc543fe4c13cd102836e1bca
kavinda dissanayake <121952332+kavinda44@users.noreply.github.com>
2025-09-15 11:42:24 +0530

    how it works section complete

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
 import Products from '../../components/Products/Products'
+import Steps from '../../components/Steps/Steps'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Navbar/>
       <Header/>
       <Products/>
+      <Steps/>
     </div>
   )
 }
