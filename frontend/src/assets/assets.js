e8ad55ad910fc24dd4bdd040328c6643e5f307ab
kavinda dissanayake <121952332+kavinda44@users.noreply.github.com>
2025-09-15 12:33:38 +0530

    popular service section complete

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -4,7 +4,14 @@ import hero_img from './hero_img.png'
 import service1 from './service1.png'
 import service2 from './service2.png'
 import service3 from './service3.png'
+import service4 from './service4.png'
+import service5 from './service5.png'
+import service6 from './service6.png'
+import service7 from './service7.png'
+import service8 from './service8.png'
+import service9 from './service9.png'         
 import step_bg from './step_bg.png'
+
 
 
 export const assets = {
@@ -13,7 +20,13 @@ export const assets = {
     service1,
     service2,
     service3,
-    step_bg
-   
+    step_bg,
+    service4,
+    service5,
+    service6,
+    service7,
+    service8,
+    service9
+    
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
 import Products from '../../components/Products/Products'
 import Steps from '../../components/Steps/Steps'
+import Services from '../../components/Services/Services'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Header/>
       <Products/>
       <Steps/>
+      <Services/>
     </div>
   )
 }
