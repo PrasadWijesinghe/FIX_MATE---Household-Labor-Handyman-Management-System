dc677d29a56ea86c3f55ed31b58fb590df830ed8
PrasadWijesinghe <wmprasadwijesinghe@gmail.com>
2025-09-16 18:38:18 +0530

    commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,13 @@
 
 import logo from './web_logo.png'
 import hero_img from './hero_img.png'
+import Upload_img from './Upload.png'
 
 
 export const assets = {
     logo,
-    hero_img
+    hero_img,
+    Upload_img
    
 }
 
