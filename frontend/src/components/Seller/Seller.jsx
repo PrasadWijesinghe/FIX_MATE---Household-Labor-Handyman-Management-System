304549dff0a445c7a53c91ff15c6d58adfd0615e
kavinda dissanayake <121952332+kavinda44@users.noreply.github.com>
2025-09-19 18:41:59 +0530

    fixed issues in services page and added register navigation

--- frontend/src/components/Seller/Seller.jsx
+++ frontend/src/components/Seller/Seller.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import { useNavigate } from 'react-router-dom';
 
 const Seller = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-center px-4 sm:px-6 md:px-10 lg:px-20 py-10">
       <div
@@ -25,30 +27,15 @@ const Seller = () => {
 
        
         <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3 sm:gap-4 mt-6 w-full">
-          <button className="w-full sm:w-auto text-sm px-5 py-2.5 bg-blue-600 text-white rounded-xl cursor-pointer hover:bg-black transition">
+          <button className="w-full sm:w-auto text-sm px-5 py-2.5 bg-blue-600 text-white rounded-xl cursor-pointer hover:bg-black transition" onClick={() => navigate('/SupplierRegister')}>
             Join as a Seller
           </button>
-          <button
+          <button 
             type="button"
-            className="group flex items-center gap-2 px-5 py-2.5 text-gray-700 hover:text-black active:scale-95 transition"
-          >
-            Learn more
-            <svg
-              className="mt-1 group-hover:translate-x-0.5 transition-all"
-              width="15"
-              height="11"
-              viewBox="0 0 15 11"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1 5.5h13.092M8.949 1l5.143 4.5L8.949 10"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            className="w-full sm:w-auto text-sm px-5 py-2.5 bg-white text-gray-800 rounded-xl cursor-pointer hover:bg-gray-100 transition" onClick={() => navigate('/vendorregister')}>
+          
+            Join as a Worker
+           
           </button>
         </div>
       </div>
